63708083837b4f09f19d3440cb38cd22c6c8cd65
Archomeda <archomeda@hotmail.com>
2015-09-27 03:32:41 +0200

    Add DLC related utilities

---
+++ CSL Common Shared/Utils/DlcUtils.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ColossalFramework.Steamworks;
+
+namespace CommonShared.Utils
+{
+    /// <summary>
+    /// Contains various utilities regarding DLCs.
+    /// </summary>
+    public static class DlcUtils
+    {
+        /// <summary>
+        /// An enumerator that contains all available DLCs.
+        /// </summary>
+        [Flags]
+        public enum Dlc
+        {
+            None = 0,
+            AfterDark = 1
+        }
+
+        /// <summary>
+        /// Gets the installed DLCs.
+        /// </summary>
+        /// <value>
+        /// The installed DLCs.
+        /// </value>
+        public static Dlc InstalledDlcs
+        {
+            get
+            {
+                Dlc dlcs = Dlc.None;
+                if (IsAfterDarkInstalled) dlcs |= Dlc.AfterDark;
+                return dlcs;
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the After Dark DLC is installed.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if After Dark is installed; otherwise, <c>false</c>.
+        /// </value>
+        public static bool IsAfterDarkInstalled { get { return Steam.IsDlcInstalled(369150); } }
+    }
+}
